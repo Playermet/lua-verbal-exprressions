0281522ecad8be8d0bca1f5ce535f99877ca642c
Playermet <playermet@gmail.com>
2016-01-18 23:33:10 +0200

    Add constructor and base methods;

--- verex.lua
+++ verex.lua
@@ -1,3 +1,17 @@
 local mod = {}
 
+mod.__index = mod
+
+function mod.new(class)
+  return setmetatable({}, class)
+end
+
+function mod:add(item)
+  table.insert(self, item)
+end
+
+function mod:add_sf(...)
+  self:add(string.format(...))
+end
+
 return mod
