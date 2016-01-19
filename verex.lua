fdfdaa297aa00a825c41faf2e39cdcabd7f688db
Playermet <playermet@gmail.com>
2016-01-19 23:16:46 +0200

    Add find, findPattern, match methods;

--- verex.lua
+++ verex.lua
@@ -1,4 +1,5 @@
 local mod = {}
+local aux = {}
 
 mod.__index = mod
 
@@ -6,12 +7,74 @@ function mod.new(class)
   return setmetatable({}, class)
 end
 
-function mod:add(item)
-  table.insert(self, item)
+function mod:add(func, ...)
+  table.insert(self, { func, ... })
 end
 
-function mod:add_sf(...)
-  self:add(string.format(...))
+
+function mod:match(str)
+  local from = 1
+  for i = 1, #self do
+    from = self[i][1](str, from, self[i])
+    if from == nil then
+      return false
+    end
+  end
+  return true
+end
+
+
+function mod:find(str)
+  self:add(aux.find, str)
+  return self
+end
+
+function mod:findPattern(str)
+  self:add(aux.findPattern, str)
+  return self
+end
+
+function mod:maybe(str)
+  self:add(aux.maybe, str)
+  return self
+end
+
+function mod:maybePattern(str)
+  self:add(aux.maybePattern, str)
+  return self
+end
+
+
+function aux.find(str, from, pattern)
+  local b,e = string.find(str, pattern[2], from, true)
+  if b == from then
+    return e + 1
+  end
+  return nil
+end
+
+function aux.findPattern(str, from, pattern)
+  local b,e = string.find(str, pattern[2], from)
+  if b == from then
+    return e + 1
+  end
+  return nil
+end
+
+function aux.maybe(str, from, pattern)
+  local b,e = string.find(str, pattern[2], from, true)
+  if b == from then
+    return e + 1
+  end
+  return from
+end
+
+function aux.maybePattern(str, from, pattern)
+  local b,e = string.find(str, pattern[2], from)
+  if b == from then
+    return e + 1
+  end
+  return from
 end
 
 return mod
